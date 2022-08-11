1f1ea7c9a06ac10f0e70ecda676356422d855afb
Anibal Hugo <ani2bal@yahoo.com>
2022-08-10 20:59:40 -0400

    updated

--- breads/server.js
+++ breads/server.js
@@ -1,28 +1,28 @@
 const express = require('express');
-
-// configuration
+// CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
-//middleware
+// MIDDLEWARE
+app.use(express.static('public'));
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views'));
-
-
-//routes
-app.get('/', (req,res) => {
-    res.send('Welcome to an awesome app about breads!');
+app.engine('jsx', require('express-react-views').createEngine());
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!');
 });
-
-
-//breads
-const breadsController = require('./controllers/breads-controller');
+// Breads
+const breadsController = require('./controllers/breads_controller.js');
 app.use('/breads', breadsController);
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404');
+});
 
-//Listen
+// LISTEN
 app.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`)
-})
+  console.log(`listening on PORT: ${PORT}`);
+});
